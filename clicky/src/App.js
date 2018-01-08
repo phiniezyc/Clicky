146293e11853857e6f1dca5af654ef7dd7111c0d
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-08 18:56:52 -0500

    imported css to main app

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
 
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        
+
       </div>
     );
   }
--- clicky/src/components/Navbar/Navbar.js
+++ clicky/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { Navbar, NavItem } from 'react-materialize';
 
 const NavBar = () => (
     <div>
-        <Navbar className="baseColor brand center" brand="logo goes here" left>
+        <Navbar className="baseColor" brand="Clicky" left>
             <NavItem href="/">Getting started
             </NavItem>
             <NavItem href="/">Components</NavItem>
