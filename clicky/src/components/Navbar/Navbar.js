85e183229b4a259620ce2e3b6f65c232929d3e80
Chance Phiniezy <phiniezyc@gmail.com>
2018-01-11 04:17:37 -0500

    working code which now consolelogs on clicks

--- clicky/src/components/Navbar/Navbar.js
+++ clicky/src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-materialize';
 
+
+
+
 const NavBar = () => (
     <div>
         <Navbar className="baseColor" brand="Clicky" left>
--- clicky/src/components/PicturesBox/Images.js
+++ clicky/src/components/PicturesBox/Images.js
@@ -1,6 +1,7 @@
 import React, { Component }from 'react';
 
-// import { Card, CardTitle } from 'react-materialize';
+
+
 
 class Images extends Component {
     constructor(props) {
@@ -14,8 +15,12 @@ class Images extends Component {
             return a;
         }
 
-        const shuffledImages = shuffle(Images);
+        const shuffledImages = shuffle(this.props.imagesToDisplay); // code that shuffles images 
+    
+    }
 
+    hello() {
+        console.log("hello");
     }
 
     render() {
@@ -23,67 +28,21 @@ class Images extends Component {
             <div>
                 {this.props.imagesToDisplay.map((image, i) => {
                     return (
-                        // <Card  id='cardSize'className='small text-darken-4'
-	                    //     header={<CardTitle image={image.image}>{image.name} </CardTitle>}
-	                    //     actions={[<a href='/'>This is a Link</a>]}>
-                        //     {/* {image.name} */}
-                        
-                        // </Card>
-                        <div key={i} className='imageBoxes'>
+                        <div key={i} className='imageBoxes' onClick={this.shuffledImages}>
                             <div className='nameDisplay'>
                             <h6 className='fontControl' key={image.name}>{image.name}</h6>
                             </div> 
                             <div>                          
-                            <img key={i} src={image.image} alt=''></img >
+                            <img key={i} src={image.image} alt=''  onClick={this.hello}></img >
                             </div> 
                         </div>
                     )
-                })
-                }
+                })}
             </div>
         )
     }
 } 
 
-// ((props) => {
-//     // function shuffle(a) {
-//     //     for (let i = a.length - 1; i > 0; i--) {
-//     //         const j = Math.floor(Math.random() * (i + 1));
-//     //         [a[i], a[j]] = [a[j], a[i]];
-//     //     }
-//     //     return a;
-//     // }
-
-//     // const shuffledImages = shuffle(Images);
-//     // console.log(shuffledImages);
     
-
-//     return (
-//             <div>
-//                 {props.imagesToDisplay.map((image, i) => {
-//                     return (
-//                         // <Card  id='cardSize'className='small text-darken-4'
-// 	                    //     header={<CardTitle image={image.image}>{image.name} </CardTitle>}
-// 	                    //     actions={[<a href='/'>This is a Link</a>]}>
-//                         //     {/* {image.name} */}
-                        
-//                         // </Card>
-//                         <div key={i} className='imageBoxes'>
-//                             <div className='nameDisplay'>
-//                             <h6 className='fontControl' key={image.name}>{image.name}</h6>
-//                             </div> 
-//                             <div>                          
-//                             <img key={i} src={image.image} alt=''></img >
-//                             </div> 
-//                         </div>
-//                     )
-//                 })
-//                 }
-//             </div>
-//         )
-// });
-    
-    
-
 
 export default Images;
